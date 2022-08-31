7775a685d719be9f9f34e428a8e03aea7e5f7107
rdwntfk <mochridwantaufikalamsyah@gmail.com>
2022-08-31 20:32:14 +0700

    add new migration table of comment like with test and linter

---
+++ tests/CommentLikesTableTestHelper.js
@@ -0,0 +1,38 @@
+/* istanbul ignore file */
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const CommentLikesTableHelper = {
+  async addCommentLike({ id = 'like-y_123', comment = 'comment-q_123', owner = 'user-123' }) {
+    const query = {
+      text: 'INSERT INTO comment_likes VALUES($1, $2, $3)',
+      values: [id, comment, owner],
+    };
+
+    await pool.query(query);
+  },
+
+  async deleteCommentLike(comment, owner) {
+    const query = {
+      text: 'DELETE FROM comment_likes WHERE comment = $1 AND owner = $2',
+      values: [comment, owner],
+    };
+
+    await pool.query(query);
+  },
+
+  async checkIsCommentLiked(comment, owner) {
+    const query = {
+      text: 'SELECT comment FROM comment_likes WHERE comment = $1 and owner = $2',
+      values: [comment, owner],
+    };
+
+    const result = await pool.query(query);
+    return result.rows;
+  },
+
+  async cleanTable() {
+    await pool.query('DELETE FROM comment_likes WHERE 1=1');
+  },
+};
+
+module.exports = CommentLikesTableHelper;
